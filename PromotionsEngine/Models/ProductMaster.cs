e30d14ed6858d6f276403a951963e3f27445e865
Abhinaw Kumar Kaushik <abhinaw@live.in>
2021-05-26 22:19:13 +0530

    Added ProductMaster Model

--- PromotionsEngine/Models/ProductMaster.cs
+++ PromotionsEngine/Models/ProductMaster.cs
@@ -4,5 +4,29 @@ namespace PromotionsEngine
 {
     public class ProductMaster
     {
+        long _productID;
+        string _productName;
+        long _quantity;
+        decimal _price;
+        public long ProductID
+        {
+            get { return _productID; }
+            set { _productID = value; }
+        }
+        public string ProductName
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+        public long Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
     }
 }
